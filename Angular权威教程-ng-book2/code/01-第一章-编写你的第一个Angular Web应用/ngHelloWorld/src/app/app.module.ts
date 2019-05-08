772db5000de403031d4fb7a7b7e9ac9c97153848
itclancode <itclancode@163.com>
2019-05-08 13:44:49 +0800

    firstChapter-userArray-1.5 【Angular权威教程】使用数组

--- Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/ngHelloWorld/src/app/app.module.ts
+++ Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/ngHelloWorld/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
