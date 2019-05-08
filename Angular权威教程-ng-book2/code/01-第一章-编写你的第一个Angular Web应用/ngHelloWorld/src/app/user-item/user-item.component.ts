d2a7028a312124f7df842ae048a7dc5cf58ea002
itclancode <itclancode@163.com>
2019-05-08 16:59:54 +0800

    firstChapter-reciveInput-1.6.2 【Angular权威教程】1.6.2-接收输入

--- Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/ngHelloWorld/src/app/user-item/user-item.component.ts
+++ Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/ngHelloWorld/src/app/user-item/user-item.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+         OnInit,
+         Input      // added this 
+} from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.css']
 })
+/* 
+* 修改了name属性,使其具有一个@input注解
+* 该语法能让我们从父模板中传进来一个值
+* 为了使用Input,我们还得把它添加到import的列表中去
+* 最后,我们不希望为name设置默认值,因此从构造函数中移除它
+* 现在我们有了一个名叫name和inut,那么该如何使用它?
+*
+*
+*/
 export class UserItemComponent implements OnInit {
-  name: string; // add name property
+  @Input() name: string; // add input annotation
+
   constructor() { 
-    this.name =  'itclanCoder';
+    // this.name =  'itclanCoder';
+    // removed setting name
   }
 
   ngOnInit() {
