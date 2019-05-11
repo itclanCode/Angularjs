75c9a5ad911473e60babc8f879ae728944de09ae
itclancode <itclancode@163.com>
2019-05-11 19:09:30 +0800

    firstChapter-addHudong-1.8.3 【Angular权威教程】添加互动-1.8.3-添加文章组件-1.8.4

--- Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/angular2-reddit/src/app/app.module.ts
+++ Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/angular2-reddit/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ArticleComponent } from './article/article.component';
+import { ArticleComponent } from './article/article.component';  // 导入新编的组件
 
 @NgModule({
-  declarations: [
+  declarations: [ // 把ArticleComponent添加到declartion列表中
     AppComponent,
-    ArticleComponent
+    ArticleComponent 
   ],
   imports: [
     BrowserModule,
@@ -20,3 +20,16 @@ import { ArticleComponent } from './article/article.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* 
+*  如果用AngularJS,可能会惊讶于本应用不知道这个新的app-article组件,这回因为在Angularjs中,指令的匹配是全局的,而Angular中,你需要明确指定要使用哪个组件(即哪个选择器)
+*  为了把这个新的ArticleComponent组件引荐给AppComponent,我们需要把Article-Component添加到NgModule的declarations列表中
+*
+* 之所以要把ArticleComponent添加到declarations中,是因为Article-Component是该模块(RedditAppModule)的一部分,然而,如果Article-Component是其他模块的一部分,可能就得通过imports来导入它了
+* 后面还会更深入的讨论NgModule,现在你只需要知道,当创建新组件时,必须同时把它放进NgModule的declarations中
+*
+*
+*
+ */
+
+
--- Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/angular2-reddit/src/app/article/article.component.ts
+++ Angular权威教程-ng-book2/code/01-第一章-编写你的第一个Angular Web应用/angular2-reddit/src/app/article/article.component.ts
@@ -4,16 +4,38 @@ import {
   Input
 } from '@angular/core';
 import { Article } from './article.model';
+/* 
+    定义了这个新组件总共用到了三部分代码
+    1. 在模板中定义了ArticleComponent的视图
+    2. 通过为类加上@Component注解定义了ArticleComponent组件的元数据
+    3. 定义了一个组件定义类(ArticleComponent),其中是组件本身的逻辑
+    
+    @Component定义了一个新组件,selector表示会用app-article标签将该组件放在页面中
 
+    在Angular中,组件的宿主就是该组件所附着到的元素,你会注意到,我们在@Component中传入了一个选项,host: {class: 'row'}
+    它告诉Angular:我们要在宿主元素(app-article)标签上设置class属性,使其具有row类
+
+    这个host选项,它意味着我们可以把app-article的页面脚本封装在组建之内,也就是说,我们不必在使用app-article标签的同时要求父视图中的页面脚本具有class=“row”属性,记住host选项,我们就可以在组建的内部配置宿主元素了
+ *
+ */
 @Component({
-  selector: 'app-article',
+  selector: 'app-article',  // 选择器的标签名,当页面被渲染出来时,这些标签仍然会留在视图中
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css'],
   host: {
-    class: 'row'
+    class: 'row' // 希望app-article都独占一行,我们使用的是Semantic UI,它提供了一个用来表示行的css类,叫做row
   }
 })
 export class ArticleComponent implements OnInit {
+  votes: number;  // 一个数字,用来表示所有赞,减去所有‘踩’的数量之和
+  title: string;  // 一个字符串,用来存放文章的标题
+  link: string;   // 一个字符串,用来存放文章的URL
+
+  constructor() { // 在constructor设置了一些默认属性
+    this.title = 'Angular2';
+    this.link = 'http://angular.io';
+    this.votes =  10;
+  }
   @Input() article: Article;
 
   voteUp(): boolean {
